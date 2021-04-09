4b75e58eb79a169b818495e82c5e0bc0739ebf84
Anton <tornblomanton@gmail.com>
2021-04-09 09:31:53 +0200

    Nano 33 iot architecture added

--- examples/ReceivingMessages/ReceivingMessages.ino
+++ examples/ReceivingMessages/ReceivingMessages.ino
@@ -3,25 +3,30 @@
 // Instance of object
 SimpleWiFiClient simpleWiFiClient;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   // Connects to WiFi and initializes UDP to listen on port 4210
-  simpleWiFiClient.init("ssid","password");
+  simpleWiFiClient.init("ssid", "password");
 
-  // Attaches SimpleWiFiClient to send the recieved message 
+  // Attaches SimpleWiFiClient to recieve messages
   // from port 4210 to the method, in this case, OnRecieved.
   simpleWiFiClient.attach(OnRecieved);
+
+  //print ip address
+  Serial.println(simpleWiFiClient.ip);
 }
 
 // Message recieved as a string.
-void OnRecieved(String message) {
+void OnRecieved(String message)
+{
   Serial.println(message);
 }
 
-void loop() {
+void loop()
+{
   // Listens for incomming messages then invokes the
   // action to send the message to the attached method.
   simpleWiFiClient.run();
-
 }
